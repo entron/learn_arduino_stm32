e3e46b7ee67472c8779a015c83d426dc02bb9c1e
Cheng Guo <cheng.guo.work@gmail.com>
2025-09-04 07:57:27 +0800

    Move serial to USART2 to save pins for simplefoc mini

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
+#include <HardwareSerial.h>
+
+// Manually instantiate Serial2 on USART2 (PA2=TX, PA3=RX) if not provided by core
+#if !defined(SERIAL_PORT_HARDWARE2) && !defined(Serial2)
+HardwareSerial Serial2(USART2);
+#endif
 
 void setup() {
     // Start serial communication
-    Serial.begin(115200);
+    Serial2.begin(115200);
     // Wait a bit for the serial monitor to connect (optional)
     delay(2000);
-    Serial.println("Blue Pill is alive!");
+    Serial2.println("Blue Pill is alive!");
 }
 
 void loop() {
     // Print uptime in milliseconds
-    Serial.print("Uptime: ");
-    Serial.print(millis());
-    Serial.println(" ms");
+    Serial2.print("Uptime: ");
+    Serial2.print(millis());
+    Serial2.println(" ms");
 
     // Wait one second
     delay(1000);
